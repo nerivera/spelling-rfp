b80f1279bee53e218c50041f47855c5be9b1a430
Sujithkumar Manoharan <sujith.vanam@gmail.com>
2022-03-29 09:21:54 -0400

    # WARNING: head commit changed in the meantime
    Merge branch 'main' of git@github.com:nerivera/spelling-rfp.git into
    main

---
+++ org/hcpss/inst/nriver5714/aood/spellingrfp/Constants.java
@@ -0,0 +1,12 @@
+package org.hcpss.inst.nriver5714.aood.spellingrfp;
+
+import java.util.function.IntUnaryOperator;
+
+public class Constants {
+	public static final String DEFAULT_CONFIG_FILE_PATH = "config.json";
+	public static final IntUnaryOperator AGE_TO_LEVEL_INDEX = age -> age / 2;
+	public static final int STREAK_TO_LEVEL_UP = 7;
+	public static final int NUM_RECENT_WORDS = 6;
+	public static final double RECENT_PROPORTION_WRONG_TO_DROP = 0.7;
+
+}
