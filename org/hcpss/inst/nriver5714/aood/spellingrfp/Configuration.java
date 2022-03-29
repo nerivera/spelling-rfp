b9dac66b1096dab3438f32d5173e3bf7a16a7f29
Nilo E Rivera <nilo.e.rivera@gmail.com>
2022-03-29 10:14:50 -0400

    Added most methods in Configuration

--- org/hcpss/inst/nriver5714/aood/spellingrfp/Configuration.java
+++ org/hcpss/inst/nriver5714/aood/spellingrfp/Configuration.java
@@ -2,7 +2,7 @@ package org.hcpss.inst.nriver5714.aood.spellingrfp;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.Map;
 
 public class Configuration {
 
@@ -10,7 +10,7 @@ public class Configuration {
 	private Configuration() {
 	}
 
-	private static Set<User> users;
+	private static Map<String, User> users;
 	private static List<Level> levels;
 
 	public static void load() {
@@ -18,25 +18,33 @@ public class Configuration {
 	}
 
 	public static void load(String configFilePath) {
-		// TODO write load method
+		// TODO do the load method
 	}
 
 	public static Optional<User> getUser(String name) {
+		return Optional.ofNullable(users.get(name));
 	}
 
-	public static User createUser(String name, int age) {
+	public static void createUser(String name, int age) {
+		if (users.containsKey(name))
+			throw new IllegalStateException("a user with the name \"" + name + "\" already exists");
+		users.put(name, new User(name, Constants.AGE_TO_LEVEL_INDEX.applyAsInt(age)));
 	}
 
 	public static Level getLevel(int levelIndex) {
+		return levels.get(levelIndex);
 	}
 
-	public static void insertLevelBefore(int levelIndex) {
+	public static void addLevelBefore(int levelIndex) {
+		levels.add(levelIndex, new Level());
 	}
 
 	public static void addLevelAfter(int levelIndex) {
+		levels.add(levelIndex + 1, new Level());
 	}
 
 	public static void removeLevel(int levelIndex) {
+		levels.remove(levelIndex);
 	}
 
 }
