d3ff3b0c682c81b7c5e05d579ce6edfcb47e684f
Nilo E Rivera <nilo.e.rivera@gmail.com>
2022-03-31 10:50:28 -0400

    Added load method to Configuration

--- org/hcpss/inst/nriver5714/aood/spellingrfp/Configuration.java
+++ org/hcpss/inst/nriver5714/aood/spellingrfp/Configuration.java
@@ -1,8 +1,15 @@
 package org.hcpss.inst.nriver5714.aood.spellingrfp;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.List;
 import java.util.Optional;
 import java.util.Map;
+import java.util.HashMap;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 public class Configuration {
 
@@ -13,12 +20,27 @@ public class Configuration {
 	private static Map<String, User> users;
 	private static List<Level> levels;
 
-	public static void load() {
+	public static void load() throws FileNotFoundException {
 		load(Constants.DEFAULT_CONFIG_FILE_PATH);
 	}
 
-	public static void load(String configFilePath) {
-		// TODO do the load method
+	public static void load(String configFilePath) throws FileNotFoundException {
+		var jsonObject = new JSONObject(new JSONTokener(new FileReader(configFilePath)));
+		users = new HashMap<>();
+		JSONArray usersArray = jsonObject.getJSONArray("users");
+		usersArray.forEach(userObject -> {
+			var userJSONObject = (JSONObject) userObject;
+			String name = userJSONObject.getString("name");
+			int levelIndex = userJSONObject.getInt("levelIndex");
+			int streak = userJSONObject.getInt("streak");
+			JSONArray recentResultsJSONArray = userJSONObject.getJSONArray("recentResults");
+			boolean[] recentResults = new boolean[recentResultsJSONArray.length()];
+			for (var i = 0; i < recentResults.length; i++) {
+				recentResults[i] = recentResultsJSONArray.getBoolean(i);
+			}
+			var user = new User(name, levelIndex, streak, recentResults);
+			users.put(name, user);
+		});
 	}
 
 	public static Optional<User> getUser(String name) {
@@ -28,6 +50,7 @@ public class Configuration {
 	public static void createUser(String name, int age) {
 		if (users.containsKey(name))
 			throw new IllegalStateException("a user with the name \"" + name + "\" already exists");
+
 		users.put(name, new User(name, Constants.AGE_TO_LEVEL_INDEX.applyAsInt(age)));
 	}
 
