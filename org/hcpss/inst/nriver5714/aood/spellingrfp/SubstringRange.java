2e6907e95a6218bc20012d0910e0ae6af44fd5f4
Sujithkumar Manoharan <sujith.vanam@gmail.com>
2022-03-31 09:48:48 -0400

    SubstringRange.java

---
+++ org/hcpss/inst/nriver5714/aood/spellingrfp/SubstringRange.java
@@ -0,0 +1,23 @@
+package org.hcpss.inst.nriver5714.aood.spellingrfp;
+
+public class SubstringRange {
+	private int beginIndex;
+	private int endIndex;
+	public SubstringRange() {
+		beginIndex = -1;
+		endIndex = -1;
+	}
+	public SubstringRange(int beginIndex, int endIndex) {
+		this.beginIndex = beginIndex;
+		this.endIndex = endIndex;
+	}
+	public boolean isEmpty() {
+		return (getBeginIndex() == -1);
+	}
+	public int getBeginIndex() {
+		return this.beginIndex;
+	}
+	public int getEndIndex() {
+		return this.endIndex;
+	}
+}
